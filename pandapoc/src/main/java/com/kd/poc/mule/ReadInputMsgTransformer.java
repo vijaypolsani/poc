6388ec37167b671b7035eeb3f1afbf95a86edfe6
user <user@rohan>
2014-01-12 15:29:32 -0800

    .

--- pandapoc/src/main/java/com/kd/poc/mule/ReadInputMsgTransformer.java
+++ pandapoc/src/main/java/com/kd/poc/mule/ReadInputMsgTransformer.java
@@ -1,10 +1,17 @@
 package com.kd.poc.mule;
 
+import org.mule.api.MuleContext;
 import org.mule.api.MuleMessage;
+import org.mule.api.annotations.expressions.Lookup;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.registry.RegistrationException;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
-public class ReadInputMsgTransformer extends AbstractMessageTransformer {
+public class ReadInputMsgTransformer extends AbstractMessageTransformer implements MuleContextAware {
+
+	@Lookup
+	private MuleContext muleContext;
 
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
@@ -17,9 +24,22 @@ public class ReadInputMsgTransformer extends AbstractMessageTransformer {
 			message.setOutboundProperty("Body", body);
 		else
 			message.setOutboundProperty("Body", "Got empty SMS Body.");
-		if (from != null)
+		if (from != null && ((String) from).trim().length() != 0) {
 			message.setOutboundProperty("From", from);
+		} else
+			message.setOutboundProperty("From", "16504096365");
+		try {
+			muleContext.getRegistry().registerObject("From", from);
+			System.out.println("Data in MuleContext " + muleContext.toString());
+		} catch (RegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return message;
 	}
 
+	public void setMuleContext(MuleContext muleContext) {
+		this.muleContext = muleContext;
+	}
 }
